578aed58d784af7862e22381cac03751da48c395
Wojtek Potocki <szkolenia@wojtekpotocki.pl>
2021-09-30 19:30:53 +0200

    init

--- src/pl/testuj/Main.java
+++ src/pl/testuj/Main.java
@@ -1,8 +1,26 @@
 package pl.testuj;
 
+
+/*
+- stworzyć klasy PrivateContact oraz WorkContact - dziedziczące
+po PhoneContact
+
+- wyświetlenie kontaktu
+- wyślij jako wizytówka zaimplementować metodę send()
+
+Wykorzystując polimorfizm stworzyć tablicę przechowującą kilka kontaktów różnego typu
+i na każdym kontakcie wywołać metodę print()
+
+
+
+ */
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        MobileContact mobileContact = new MobileContact(1,
+                "Wojtek", "Nowak", "123456789");
+
+        mobileContact.print();
     }
 }
